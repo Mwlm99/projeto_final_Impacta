fb56cb9d7ffa4d2df21039620d51f6b9720056ca
Mwlm99 <mwlm1122@live.com>
2024-02-23 17:02:39 -0300

    etl inicial

---
+++ ETL/ETL_Dataset.py
@@ -0,0 +1,3 @@
+import pandas as pd
+covid_geral = pd.read_csv('C:/Users/mwlm1/OneDrive/Área de Trabalho/Projeto_final_Impacta/Datasets/Dados_SP/dados_covid_sp.csv/dados_covid_sp.csv')
+print(covid_geral)
